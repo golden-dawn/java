38f220b647989406e749430b8840de8ba391b20c
golden-dawn <constantin_m_adam@yahoo.com>
2018-07-11 03:05:35 -0400

    Added OBV calculation

--- indicators/StxUDV.java
+++ indicators/StxUDV.java
@@ -18,22 +18,30 @@ public class StxUDV<T extends StxRec> {
 	ArrayList<Float> res = new ArrayList<Float>();
 	float tot_vol = 0, sum_vol = 0;
 	float tot_day = 0, sum_day = 0;
-	float diff;
+	float obv = 0, diff;
 	int ix = start;
 	while(ix <= end) {
 	    StxRec sr = data.get(ix);
 	    diff = sr.c - data.get(ix - 1).c;
-	    if(diff > 0)
+	    if(diff > 0) {
 		sum_day+= 1;
-	    else
+		obv += sr.v;
+	    } else {
 		sum_day -= 1;
+		obv -= sr.v;
+	    }
+	    // String s = String.format
+	    // 	("%s: c = %.2f, c_1 = %.2f, diff = %.2f, v = %.0f, obv = %.0f",
+	    // 	 sr.date, sr.c, data.get(ix - 1).c, diff, sr.v, obv);
+	    // System.err.println(s);
 	    tot_day += 1;
 	    tot_vol += sr.v * Math.abs(diff);
 	    sum_vol += sr.v * diff;
 	    ix++;
 	}
 	res.add(sum_vol / tot_vol);
-	res.add(sum_day / tot_day);
+	res.add(sum_day);
+	res.add(obv);
 	return res;
     }
 }
