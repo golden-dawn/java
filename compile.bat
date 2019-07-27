c38531d63ba31ac9ffabd8138f60798de3175b32
golden-dawn <constantin_m_adam@yahoo.com>
2019-07-26 22:50:57 -0400

    Compile on windows

---
+++ compile.bat
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "creating output directory"
+mkdir -p output
+echo "compiling core"
+cd core/
+javac *.java -d ../output
+echo "compiling indicators"
+cd ../indicators/
+javac -cp ../output/ *.java -d ../output
+echo "compiling jl"
+cd ../jl/
+javac -cp ../output/ *.java -d ../output
+echo "compiling gui"
+cd ../gui/
+javac -cp ../output/ *.java -d ../output
+cd ..
