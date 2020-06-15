1f5cdb7d91d56e9c63c4975bdc7d5e12d436be4f
golden-dawn <constantin_m_adam@yahoo.com>
2020-06-14 23:27:52 -0400

    Untabify

--- indicators/StxOBV.java
+++ indicators/StxOBV.java
@@ -24,24 +24,24 @@ public class StxOBV {
     }
 
     public float obv(int start_jl, int end) {
-// 	System.err.printf("%.1f: obv() start = %d end = %d\n", 
-// 			  sjl.getFactor(), start_jl, end);
-	int jls = Math.abs(start_jl);
-	float obv = 0;
-	StxJL start_rec = sjl.data(jls);
-	int start = data.find(start_rec.date, 0);
-	StxRec srs = data.get(start), sre = data.get(end);
-	int jle = sjl.recs().find(sre.date, 0);
-	StxJL end_rec = sjl.data(jle);
-	int start_piv_state = (start_jl > 0)? start_rec.s: start_rec.s2;
-	if ((sjl.up(start_piv_state) && srs.hiB4Lo()) || 
-	    (sjl.dn(start_piv_state) && !srs.hiB4Lo()))
-	    obv += start_rec.obv2 / start_rec.vr;
-	obv += start_rec.obv3 / start_rec.vr;
-	for(int ix = jls + 1; ix <= jle; ix++) {
-	    StxJL jlr = sjl.data(ix);
-	    obv += (jlr.obv1 + jlr.obv2 + jlr.obv3) / jlr.vr;
-	}
-	return obv;
+//      System.err.printf("%.1f: obv() start = %d end = %d\n", 
+//                        sjl.getFactor(), start_jl, end);
+        int jls = Math.abs(start_jl);
+        float obv = 0;
+        StxJL start_rec = sjl.data(jls);
+        int start = data.find(start_rec.date, 0);
+        StxRec srs = data.get(start), sre = data.get(end);
+        int jle = sjl.recs().find(sre.date, 0);
+        StxJL end_rec = sjl.data(jle);
+        int start_piv_state = (start_jl > 0)? start_rec.s: start_rec.s2;
+        if ((sjl.up(start_piv_state) && srs.hiB4Lo()) || 
+            (sjl.dn(start_piv_state) && !srs.hiB4Lo()))
+            obv += start_rec.obv2 / start_rec.vr;
+        obv += start_rec.obv3 / start_rec.vr;
+        for(int ix = jls + 1; ix <= jle; ix++) {
+            StxJL jlr = sjl.data(ix);
+            obv += (jlr.obv1 + jlr.obv2 + jlr.obv3) / jlr.vr;
+        }
+        return obv;
     }
 }
