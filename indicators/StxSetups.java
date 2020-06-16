1a87bab849825f08fa01026f22de3a956cf8f643
golden-dawn <constantin_m_adam@yahoo.com>
2020-06-15 23:34:23 -0400

    Untabify

--- indicators/StxSetups.java
+++ indicators/StxSetups.java
@@ -7,8 +7,8 @@ public class StxSetups extends StxRecord {
     public String setups;
 
     public StxSetups(String dt, String setups) {
-	super(dt, 0);
-	this.setups = setups;
+        super(dt, 0);
+        this.setups = setups;
     }
     public void split(float s) {}
     public void dividend(float d) {}
