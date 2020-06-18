c02c577a1fdc04013014ecf7ceb28dd4d06d31d6
golden-dawn <constantin_m_adam@yahoo.com>
2020-06-17 22:33:37 -0400

    Untabify

--- indicators/StxxSetups.java
+++ indicators/StxxSetups.java
@@ -16,45 +16,45 @@ public class StxxSetups {
     private String stk;
     
     public StxxSetups(String stk) {
-	recs = new StxData<StxSetups>();
-	this.stk = stk;
-	BufferedReader br = null;
-	try {
-	    String line;
-	    String fname = String.format("/home/cma/setups/%s.csv", stk);
-	    br = new BufferedReader(new FileReader(fname));
-	    while ((line = br.readLine()) != null) {
-		String[] tokens = line.split(";");
-		String dt = tokens[0].trim();
-		// System.err.printf("line = %s, len(tokens) = %d, dt = %s\n",
-		// 		  line, tokens.length, dt);
-		String sss = (tokens.length > 1) ? tokens[1].trim(): "";
-		recs.add(new StxSetups(dt, sss));
-	    }
-	} catch (Exception e) {
-	    // System.err.println(e.toString());
-	    // e.printStackTrace();
-	} finally {
-	    try {
-		if (br != null)
-		    br.close();
-	    } catch (Exception ex) {
-		ex.printStackTrace();
-	    }
-	}
+        recs = new StxData<StxSetups>();
+        this.stk = stk;
+        BufferedReader br = null;
+        try {
+            String line;
+            String fname = String.format("/home/cma/setups/%s.csv", stk);
+            br = new BufferedReader(new FileReader(fname));
+            while ((line = br.readLine()) != null) {
+                String[] tokens = line.split(";");
+                String dt = tokens[0].trim();
+                // System.err.printf("line = %s, len(tokens) = %d, dt = %s\n",
+                //                line, tokens.length, dt);
+                String sss = (tokens.length > 1) ? tokens[1].trim(): "";
+                recs.add(new StxSetups(dt, sss));
+            }
+        } catch (Exception e) {
+            // System.err.println(e.toString());
+            // e.printStackTrace();
+        } finally {
+            try {
+                if (br != null)
+                    br.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
     }
     public String getSetups(String dt) {
-	if(recs.size() == 0)
-	    return "";
-	int ix = recs.find(dt, 0);
-	StringBuffer sb = new StringBuffer();
-	if(ix > 1)
-	    sb.append("T-2: ").append(recs.get(ix - 2).setups).append("\n");
-	if(ix > 0)
-	    sb.append("T-1: ").append(recs.get(ix - 1).setups).append("\n");
-	if(ix >= 0)
-	    sb.append("  T: ").append(recs.get(ix).setups).append("\n");
-	return  sb.toString();
+        if(recs.size() == 0)
+            return "";
+        int ix = recs.find(dt, 0);
+        StringBuffer sb = new StringBuffer();
+        if(ix > 1)
+            sb.append("T-2: ").append(recs.get(ix - 2).setups).append("\n");
+        if(ix > 0)
+            sb.append("T-1: ").append(recs.get(ix - 1).setups).append("\n");
+        if(ix >= 0)
+            sb.append("  T: ").append(recs.get(ix).setups).append("\n");
+        return  sb.toString();
     }
 
     public String getStk() { return stk; }
@@ -64,7 +64,7 @@ public class StxxSetups {
         new StxCal(c.get(Calendar.YEAR) + 1);
         String dt = args[1], stk = args[0];
         StxxSetups sss = new StxxSetups(stk);
-	System.err.printf("dt = %s, stk = %s\n", dt, stk);
+        System.err.printf("dt = %s, stk = %s\n", dt, stk);
         String res = sss.getSetups(dt);
         System.err.println(res);
     }    
